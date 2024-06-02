db46c3a37e1ef8df08cb01d3f4617a3bd38425e7
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-06-02 00:24:47 -0300

    feat: add analytics

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
+
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
