debce5973ca45ce2a13ebb06e03ee0fcc0b3ffa8
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-23 02:59:28 -0300

    fix: adjust layout and padding for improved responsiveness in Home component

--- docs/src/app/[locale]/page.tsx
+++ docs/src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
 	return (
 		<main className="mx-auto w-full bg-gradient-to-br from-blue-50 via-white to-purple-50 px-4 py-12">
-			<div className="rounded-xl bg-white p-6 shadow-md sm:p-10">
+			<div className="m-auto w-full rounded-xl bg-white p-2 shadow-md sm:p-8 xl:max-w-[1780px]">
 				<h2 className="mb-6 text-center text-2xl font-bold text-gray-800">
 					{scopedT("Test the translator")}
 				</h2>
